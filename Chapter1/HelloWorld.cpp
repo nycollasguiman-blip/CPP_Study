572d6017a906b76f8b05755e597ea3e4d235020b
Nycollas <nycollas.guiman@gmail.com>
2026-03-18 19:01:47 -0300

    First hours of reading

---
+++ Chapter1/HelloWorld.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+inline void keep_window_open() { char ch; cin>>ch; }
+
+//Esse programa simplesmente escreva "Hello World" na tela
+int main()
+{
+    std::cout << "Hello World!\n";
+    keep_window_open();
+    return 0;
+
+} //Testando os comentários
+/*
+
+Testando outro tipo de comentário
+
+*/
