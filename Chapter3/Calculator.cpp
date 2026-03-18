9328680f04f7557b704c6cdc4635bf5eb17bb5ae
Nycollas <nycollas.guiman@gmail.com>
2026-03-18 20:14:51 -0300

    Repairing UTF-8

--- Chapter3/Calculator.cpp
+++ Chapter3/Calculator.cpp
@@ -4,27 +4,37 @@
 #include<algorithm>
 #include<cmath>
 #include<cstdlib>
+
+// Esse programa simplesmenta pergunda o nome e dois valores, depois mostra as 4 operações entre eles 
+
+#include <windows.h> //Consertando os caracteres especiais
+
 using namespace std;
 inline void keep_window_open() { char ch; cin>>ch; }
 
 int main()
 {
+
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
     system("cls");
+
+
     cout << "Digite o seu primeiro nome\n";
 
     string name;
     cin >> name;
 
     cout << "Digite dois números:\n";
-    int a;
-    int b;
+    double a;
+    double b;
     cin >> a >> b;
 
     cout << "Para "<< a << " e " << b << ":\n";
     cout << "Soma: " << a+b << "\n"
             << "Subtração: " << a-b << "\n"
             << "Multiplicação: " << a*b << "\n"
-            << "Divisão: " << (double)a/b;
+            << "Divisão: " << a/b;
             
 }
 
